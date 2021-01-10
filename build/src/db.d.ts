9916ca38cbf5246967501d8163ea870047c94395
Will Lane <williamlane923@gmail.com>
2021-01-10 10:30:41 -0800

    updated gitignore

--- build/src/db.d.ts
+++
@@ -1,5 +0,0 @@
-import { TodoI } from './models';
-export default class DB {
-    constructor();
-    addTodo({ title, description }: TodoI): Promise<void>;
-}
--- build/src/models.d.ts
+++
@@ -1,6 +0,0 @@
-import * as mongoose from 'mongoose';
-export declare const Todo: mongoose.Model<mongoose.Document<any>>;
-export interface TodoI {
-    title: string;
-    description: string;
-}
